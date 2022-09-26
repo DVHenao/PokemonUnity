b24b2492ca8ad2a561f05aaccaeea228b5b1cba6
DVHenao <ehenao094@gmail.com>
2022-09-26 15:31:03 -0400

    part of exercise 2 done NOT FINAL

--- Pokemon Onyx/Assets/Scripts/EncounterController.cs
+++ Pokemon Onyx/Assets/Scripts/EncounterController.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EncounterController : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject MainCamera;
+    [SerializeField]
+    public GameObject Canvas;
+    [SerializeField]
+    public GameObject EventSystem;
+
+    public GameObject MainPokemon;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int whatPokemon = Random.Range(1, 6);
+
+        switch (whatPokemon) // load random pokemon
+        { 
+            case 1:
+                //MainPokemon.AddComponent()
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+
+        }
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+     
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown("escape"))
+        {
+            MainCamera.SetActive(false);
+            Canvas.SetActive(false);
+            EventSystem.SetActive(false);
+        }
     }
 }
