3cf552aba2de227375ad3af1678a4064c01d557e
DVHenao <ehenao094@gmail.com>
2022-09-27 23:14:31 -0400

    Exercise 3 complete! Part A only

---
+++ Pokemon Onyx/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playGame()
+    {
+        Debug.Log("pressed");
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("pressed");
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
